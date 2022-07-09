d14b9a30f34f264c9c6c21e6a9563a9f2c0d9b62
Dimas Ananda <traperwaze@gmail.com>
2022-07-09 09:42:23 +0700

    add EnvCfg in config.go
    it reads and unmarshals the .env to memory
    
    Signed-off-by: Dimas Ananda <traperwaze@gmail.com>

--- config/config.go
+++ config/config.go
@@ -5,11 +5,13 @@ import (
 )
 
 var (
-	Cfg *Config
+	Cfg    *Config
+	EnvCfg *EnvConfig
 )
 
 func init() {
 	Cfg = New()
+	EnvCfg = NewEnv()
 }
 
 type Config struct {
@@ -49,6 +51,41 @@ func (c *Config) LoadConfig(paths ...string) error {
 	}
 
 	if err := c.v.Unmarshal(c); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type EnvConfig struct {
+	// Data source name for GORM. see https://gorm.io/docs/connecting_to_the_database.html#PostgreSQL
+	DSN string
+	v   *viper.Viper
+}
+
+// create new EnvConfig
+func NewEnv() *EnvConfig {
+	ec := &EnvConfig{
+		v: viper.New(),
+	}
+
+	ec.v.SetConfigType("env")
+	ec.v.SetConfigName(".env")
+	ec.v.AddConfigPath(".")
+	ec.v.AutomaticEnv()
+	ec.v.ReadInConfig()
+
+	return ec
+}
+
+func LoadEnv() error { return EnvCfg.LoadConfig() }
+
+func (ec *EnvConfig) LoadConfig() error {
+	if err := ec.v.ReadInConfig(); err != nil {
+		return err
+	}
+
+	if err := ec.v.Unmarshal(ec); err != nil {
 		return err
 	}
 
